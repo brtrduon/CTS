7b7f7ef0f45d85ce5aa34a2204ed3123797fdd70
brtrduon <brianduongt@gmail.com>
2019-04-28 15:08:59 -0700

    more bio stuff

--- client/src/components/users/index.js
+++ client/src/components/users/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+// import { Link } from 'react-router-dom'
 import './index.css'
 
 class UserIndex extends React.Component {
@@ -10,8 +10,6 @@ class UserIndex extends React.Component {
                     <h1>Carb Tech Solutions</h1>
                 </div>
                 <div className='row centered banner'>
-                    {/* <img className='banner' src='./../../../assets/img/banner.jpg' alt='banner.jpg' /> */}
-                    {/* Some header / image goes here ("featured items" ?) */}
                 </div>
                 <div className='ui middle aligned stackable grid container'>
                     <div className='row centered'>
@@ -27,17 +25,29 @@ class UserIndex extends React.Component {
                         <div className='three wide column'>Two</div>
                         <div className='three wide column'>Three</div>
                     </div>
-                    <div className='row centered'>
-                        <img src='./../../../assets/img/profile1.jpg' alt='profile1.jpg' />
-                        Founded in 1985, the idea of creating an online business specializing in carburetor rebuild kits came to Jeong-Yul Kim while he was operating and managing a local lawn equipment repair business. Having already been in the business several years prior, Mr. Kim realized that while original lawn equipment body and engine parts were easier to come by and purchase, finding proper carburetor rebuild kits for each respective carburetor style was much more difficult. As a result of the dot com boom in the mid-late 1990s and early 2000s, Mr. Kim decided to expand his business online to not only assist local homeowner and commercial customers, but to also provide service to other lawn equipment repair businesses internationally who may be experiencing the same difficulties that Mr. Kim himself experienced.
+                    <div className='row'>
+                        <div className='five wide column'>
+                            <img src='./../../../assets/img/profile1.jpg' alt='profile1.jpg' />
+                        </div>
+                        <div className='eight wide column'>
+                            <p>
+                                Founded in 1985, the idea of creating an online business specializing in carburetor rebuild kits came to Jeong-Yul Kim while he was operating and managing a local lawn equipment repair business. Having already been in the business several years prior, Mr. Kim realized that while original lawn equipment body and engine parts were easier to come by and purchase, finding proper carburetor rebuild kits for each respective carburetor style was much more difficult. As a result of the dot com boom in the mid-late 1990s and early 2000s, Mr. Kim decided to expand his business online to not only assist local homeowner and commercial customers, but to also provide service to other lawn equipment repair businesses internationally who may be experiencing the same difficulties that Mr. Kim himself experienced.
+                            </p>
+                        </div>
+                    </div>
+                    <div className='row'>
+                        <div className='five wide column'>
+                            <img src='./../../../assets/img/camera_shy.jpg' alt='camera_shy.jpg' />
+                        </div>
+                        <div className='eight wide column'>
+                            <p>
+                                Born in Seoul, South Korea, and raised in Southern California, Jared Jiho Kim obtained his Bachelor's degree in financing at the University of Utah, and obtained his Master's degree in Business Administration at the University of California, Berkeley. Through previous career experiences and technological prowess, Jared pioneered and appealed CTS to more diverse groups of lawn equipment users, ranging from homeowners to commercial users. Building upon his idealogy of tactical execution, Jared continues to implement innovative strategies to develop an alluent, yet friendly, company objective.
+                            </p>
+                        </div>
                     </div>
                     <div className='row centered'>
-                        {/* insert image here */}
-
-                        
                     </div>
                 </div>
-
             </div>
         )
     }
