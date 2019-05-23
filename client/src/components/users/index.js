779e2015ab673ed02a84985da79b225b12afac6c
brtrduon <brianduongt@gmail.com>
2019-05-23 13:55:04 -0700

    grid restructure. DRY

--- client/src/components/users/index.js
+++ client/src/components/users/index.js
@@ -17,8 +17,8 @@ class UserIndex extends React.Component {
         }
 
         return (
-            <div className='featured_item'>
-                <h3>Featured Item</h3>
+            <div>
+                <h5>Featured Item</h5>
                 <h5><Link to={`/item/${item._id}`}>{item.name}</Link></h5>
                 <h5>{item.description}</h5>
                 <h5>Item number: {item.item_number}</h5>
@@ -36,39 +36,31 @@ class UserIndex extends React.Component {
                         <h1 className='title'>Carb Tech Solutions</h1>
                     </div>
                 </div>
-                {/* <div className='ui middle aligned stackable grid container'> */}
                 <div className='ui stackable grid'>
-                    <div className='row centered'>
+                    <div className='ui row bottom attached'>
+                        <h5 className='about'>
+                            Carb Tech Solutions (CTS) is an online retailer and distributor that specializes in carburetor rebuild kits. These kits include (but are not limited to) metering and pump diaphragms, inlet needle values, and entire carburetor assemblies. Our goal is to provide high quality aftermarket alternatives comparable to those of original manufactured parts and accessories.
+                        </h5>
+                    </div>
+                    {/* <div className='ui bottom attached'>
                         <div className='three wide column black'>
-                            {/* dummy img? either put a generic img due to different stuff being pulled if db is being added when deploying or work on img upload (the latter optioon will take longer to do) */}
                             {this.featuredItem()}
                         </div>
-                        <div className='four wide column grey'>
-                            <h2>
-                                {/* background img? static img? parallax scrolling? */}
-                                Our quality carburetor rebuild kits guarantee that you will have your equipments running like new!
-                            </h2>
-                        </div>
-                        <div className='four wide column'>
+                    </div> */}
+                    <div className='ui row bottom attached'>    
+                        <div className='ui row success_stories'>
                             <h1>Success Stories</h1>
-                            {/* insert some dummy img here */}
                             <h5>
                                 "Carb Tech Solutions provides us with a do-it-yourself affordable types of products that allows us to fix our equipments without having to bring our equipments into repair shops. We are glad that there is an alternative to having to make that long trip to get our lawn equipments running again!"
                             </h5>
-                            {/* insert some dummy img here */}
                             <h5>
                                 "As a commercial gardener, these guys provide quality parts at a low price to quickly get our machines up and running. Shipping times may vary, but these guys typically ensure our orders get to us within three to five business days. Would definitely recommend to anyone who is willing to fix stuff themselves."
                             </h5>
                         </div>
                     </div>
                     <div className='row centered'>
-                        <h5 className='about'>
-                            Carb Tech Solutions (CTS) is an online retailer and distributor that specializes in carburetor rebuild kits. These kits include (but are not limited to) metering and pump diaphragms, inlet needle values, and entire carburetor assemblies. Our goal is to provide high quality aftermarket alternatives comparable to those of original manufactured parts and accessories.
-                        </h5>
-                    </div>
-                    <div className='row centered'>
                         <div className='five wide column items'>
-                            {/* change font type (?) */}
+                            {/* change font style (?) */}
                             <h5><Link className='link' to='/items'>View our affordable, quality items.</Link></h5>
                         </div>
                         <div className='five wide column contact'>
@@ -80,9 +72,9 @@ class UserIndex extends React.Component {
                             <img src='./../../../assets/img/profile1.jpg' alt='profile1.jpg' />
                         </div>
                         <div className='eight wide column'>
-                            <p>
+                            <h5>
                                 Founded in 1985, the idea of creating an online business specializing in carburetor rebuild kits came to Jeong-Yul Kim while he was operating and managing a local lawn equipment repair business. Having already been in the business several years prior, Mr. Kim realized that while original lawn equipment body and engine parts were easier to come by and purchase, finding proper carburetor rebuild kits for each respective carburetor style was much more difficult. As a result of the dot com boom in the mid-late 1990s and early 2000s, Mr. Kim decided to expand his business online to not only assist local homeowner and commercial customers, but to also provide service to other lawn equipment repair businesses internationally who may be experiencing the same difficulties that Mr. Kim himself experienced.
-                            </p>
+                            </h5>
                         </div>
                     </div>
                     <div className='row'>
@@ -90,9 +82,9 @@ class UserIndex extends React.Component {
                             <img src='./../../../assets/img/camera_shy.jpg' alt='camera_shy.jpg' />
                         </div>
                         <div className='eight wide column'>
-                            <p>
+                            <h5>
                                 Born in Seoul, South Korea, and raised in Southern California, Jared Jiho Kim obtained his Bachelor's degree in financing at the University of Utah, and obtained his Master's degree in Business Administration at the University of California, Berkeley. Through previous career experiences and technological prowess, Jared appealed and expanded CTS to more diverse groups of lawn equipment users, ranging from small home users to commercial users. Building upon his idealogy of tactical execution, Jared continues to implement effective and innovative strategies to develop towards an alluent, yet friendly, company objective.
-                            </p>
+                            </h5>
                         </div>
                     </div>
                     <div className='row centered'>
